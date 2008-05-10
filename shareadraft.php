15c551c0a540d55f62dfd65e454fccc883fa7482
Nikolay Bachiyski <nb@nikolay.bg>
2008-05-10 13:03:46 +0000

    1.0
    git-svn-id: http://plugins.svn.wordpress.org/shareadraft/trunk@45088 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- shareadraft.php
+++ shareadraft.php
@@ -4,7 +4,7 @@ Plugin Name: Share a Draft
 Plugin URI: http://wordpress.org/extend/plugins/shareadraft/
 Description: Let your friends preview one of your drafts, without giving them permissions to edit posts in your blog
 Author: Nikolay Bachiyski
-Version: 0.8
+Version: 1.0
 Author URI: http://nikolay.bg/
 Generated At: www.wp-fun.co.uk;
 */ 
@@ -356,7 +356,6 @@ SELECT;
 			jQuery(function($) {
 				$('form.shareadraft-extend').hide();
 				$('a.shareadraft-extend').show();
-				//$('a.shareadraft-extend').css('display', 'inline');
 				$('a.shareadraft-extend-cancel').show();
 				$('a.shareadraft-extend-cancel').css('display', 'inline');
 			});
